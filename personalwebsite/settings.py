aa866fd24c77e478366fefdeb9aeaa18434e8a06
Nardiena Althafia Pratama <nardienapratama@yahoo.com>
2020-06-10 01:33:09 +1000

    made changes to staticfiles settings

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -147,7 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    # os.path.join(BASE_DIR, '"static"'),
+    os.path.join(BASE_DIR, 'mysite/static')
+    # '/var/www/static/',
 ]
